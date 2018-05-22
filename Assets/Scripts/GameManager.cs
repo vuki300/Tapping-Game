9b4e9bd7875c21dc0a5321c24095b5f2867413db
vuki <vuk3003@Gmail.com>
2018-05-22 12:59:26 +0200

    update5
    done with this project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -33,17 +33,17 @@ public class GameManager : MonoBehaviour
     public GameObject StoreButton;
     public GameObject Store1;
     private int money;
-    private int clickMP;
-    private int clickINC;
-    private int idleMP;
-    private int idleINC;
-    private int bulk;
+    private int clickMP = 1;
+    private int clickINC = 5;
+    private int idleMP = 1;
+    private int idleINC = 0;
+    private int bulk = 1;
+    private int price01 = 100;
+    private int owned01;
+    private int price11 = 1000;
+    private int owned11;
     void Start()
     {
-        clickMP = 1;
-        clickINC = 5;
-        idleMP = 1;
-        idleINC = 0;
         Select.SetActive(true);
         MainScene.SetActive(false);
         Store.SetActive(false);
@@ -54,7 +54,22 @@ public class GameManager : MonoBehaviour
         idleINCText.text = "Per Frame: " + idleINC;
         idleMPText.text = "Idle Multiplier: " + idleMP + "X";
         bulkText.text = "Selected: " + bulk;
-
+        Store01Text.text = owned01 + " | $" + price01 * bulk;
+        Store02Text.text = "To be added";
+        Store03Text.text = "To be added";
+        Store04Text.text = "To be added";
+        Store05Text.text = "To be added";
+        Store06Text.text = "To be added";
+        Store07Text.text = "To be added";
+        Store08Text.text = "To be added";
+        Store11Text.text = owned11 + " | $" + price11 * bulk;
+        Store12Text.text = "To be added";
+        Store13Text.text = "To be added";
+        Store14Text.text = "To be added";
+        Store15Text.text = "To be added";
+        Store16Text.text = "To be added";
+        Store17Text.text = "To be added";
+        Store18Text.text = "To be added";
 
     }
 // Update per frame
@@ -104,17 +119,46 @@ public class GameManager : MonoBehaviour
     {
         bulk = 1;
         bulkText.text = "Selected: " + bulk;
+        Store01Text.text = owned01 + " | $" + price01 * bulk;
+        Store11Text.text = owned11 + " | $" + price11 * bulk;
     }
     public void bulk10 ()
     {
         bulk = 10;
         bulkText.text = "Selected: " + bulk;
+        Store01Text.text = owned01 + " | $" + price01 * bulk;
+        Store11Text.text = owned11 + " | $" + price11 * bulk;
     }
     public void bulk100 ()
     {
         bulk = 100;
         bulkText.text = "Selected: " + bulk;
+        Store01Text.text = owned01 + " | $" + price01 * bulk;
+        Store11Text.text = owned11 + " | $" + price11 * bulk;
     }
-
+    public void store01()
+    {
+        if(price01 * bulk <= money)
+        {
+            clickINC += 5 * bulk;
+            money -= price01 * bulk;
+            price01 += ((price01/100)*10) * bulk;
+            owned01 += bulk;
+            Store01Text.text = owned01 + " | $" + price01 * bulk;
+            clickINCText.text = "Per Click: " + clickINC;
+        }
+    }
+    public void store11()
+    {
+        if(price11 * bulk <= money)
+        {
+            clickMP += 1 * bulk;
+            money -= price11 * bulk;
+            price11 += ((price01 / 100) * 10) * bulk;
+            owned11 += bulk;
+            Store11Text.text = owned11 + " | $" + price11 * bulk;
+            clickMPText.text = "Click Multiplier: " + clickMP + "X";
+        }
+    }
        
 }
